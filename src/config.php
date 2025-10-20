9539df66569f8075e9d28f94a98f43b34d141845
BUBICH <79073551+BUBICH3228@users.noreply.github.com>
2025-10-20 13:52:15 +0700

    -

--- src/config.php
+++ src/config.php
@@ -1,17 +1,16 @@
 <?php
 session_start();
 
-$host = 'MySQL-8.4';
+$host = 'mysql';
 $dbname = 'auth_system';
-$user = 'root';
-$password = '';
-$port = 3306;
+$user = 'user';
+$password = 'password';
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname", $user, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $pdo->exec("SET NAMES utf8");
 } catch(PDOException $e) {
-    die("Ошибка БД: " . $e->getMessage());
+    die("Ошибка подключения: " . $e->getMessage());
 }
 ?>
